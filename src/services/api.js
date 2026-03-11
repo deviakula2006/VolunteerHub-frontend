78cbc13e6bfe36b8cc5d0e0ff94ede6f4c1c1b3b
AKULA DEVI GANGA BHAVANI <23A91A0501@aec.edu.in>
2026-03-11 18:41:31 +0530

    Update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Point to the local backend server you just started instead of production
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://volunteer-hub-backend-5z2n.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
